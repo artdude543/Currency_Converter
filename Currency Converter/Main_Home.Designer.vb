c421cea4d537bb7b64b1a3e697ad2ad2ee237c7c
Kieron Richardson <artdude543@hotmail.co.uk>
2013-09-23 21:07:14 +0100

    Initial Commit

---
+++ Currency Converter/Main_Home.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main_Home
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grpInput = New System.Windows.Forms.GroupBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.menuHome = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.grpOutput = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.txtuserFirstName = New System.Windows.Forms.TextBox()
+        Me.cmbTitle = New System.Windows.Forms.ComboBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtuserSurname = New System.Windows.Forms.TextBox()
+        Me.lbluserSecondName = New System.Windows.Forms.Label()
+        Me.grpInput.SuspendLayout()
+        Me.menuHome.SuspendLayout()
+        Me.grpOutput.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'grpInput
+        '
+        Me.grpInput.Controls.Add(Me.lbluserSecondName)
+        Me.grpInput.Controls.Add(Me.txtuserSurname)
+        Me.grpInput.Controls.Add(Me.Label2)
+        Me.grpInput.Controls.Add(Me.lblTitle)
+        Me.grpInput.Controls.Add(Me.cmbTitle)
+        Me.grpInput.Controls.Add(Me.txtuserFirstName)
+        Me.grpInput.Controls.Add(Me.btnConvert)
+        Me.grpInput.Location = New System.Drawing.Point(12, 27)
+        Me.grpInput.Name = "grpInput"
+        Me.grpInput.Size = New System.Drawing.Size(340, 418)
+        Me.grpInput.TabIndex = 0
+        Me.grpInput.TabStop = False
+        Me.grpInput.Text = "Input Values"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(232, 389)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 1
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'menuHome
+        '
+        Me.menuHome.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.menuHome.Location = New System.Drawing.Point(0, 0)
+        Me.menuHome.Name = "menuHome"
+        Me.menuHome.Size = New System.Drawing.Size(691, 24)
+        Me.menuHome.TabIndex = 1
+        Me.menuHome.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CloseToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'grpOutput
+        '
+        Me.grpOutput.Controls.Add(Me.Label1)
+        Me.grpOutput.Controls.Add(Me.btnReset)
+        Me.grpOutput.Location = New System.Drawing.Point(358, 27)
+        Me.grpOutput.Name = "grpOutput"
+        Me.grpOutput.Size = New System.Drawing.Size(321, 418)
+        Me.grpOutput.TabIndex = 1
+        Me.grpOutput.TabStop = False
+        Me.grpOutput.Text = "Output Values"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Total Before Commission"
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(232, 389)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 0
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'txtuserFirstName
+        '
+        Me.txtuserFirstName.Location = New System.Drawing.Point(224, 33)
+        Me.txtuserFirstName.Name = "txtuserFirstName"
+        Me.txtuserFirstName.Size = New System.Drawing.Size(100, 20)
+        Me.txtuserFirstName.TabIndex = 2
+        '
+        'cmbTitle
+        '
+        Me.cmbTitle.FormattingEnabled = True
+        Me.cmbTitle.Items.AddRange(New Object() {"Mr", "Mrs", "Miss", "Sir", "Master", "Dr"})
+        Me.cmbTitle.Location = New System.Drawing.Point(72, 33)
+        Me.cmbTitle.Name = "cmbTitle"
+        Me.cmbTitle.Size = New System.Drawing.Size(80, 21)
+        Me.cmbTitle.TabIndex = 3
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Location = New System.Drawing.Point(36, 36)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(30, 13)
+        Me.lblTitle.TabIndex = 4
+        Me.lblTitle.Text = "Title:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(161, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Forename:"
+        '
+        'txtuserSurname
+        '
+        Me.txtuserSurname.Location = New System.Drawing.Point(72, 61)
+        Me.txtuserSurname.Name = "txtuserSurname"
+        Me.txtuserSurname.Size = New System.Drawing.Size(252, 20)
+        Me.txtuserSurname.TabIndex = 6
+        '
+        'lbluserSecondName
+        '
+        Me.lbluserSecondName.AutoSize = True
+        Me.lbluserSecondName.Location = New System.Drawing.Point(14, 64)
+        Me.lbluserSecondName.Name = "lbluserSecondName"
+        Me.lbluserSecondName.Size = New System.Drawing.Size(52, 13)
+        Me.lbluserSecondName.TabIndex = 7
+        Me.lbluserSecondName.Text = "Surname:"
+        '
+        'Main_Home
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(691, 457)
+        Me.Controls.Add(Me.grpOutput)
+        Me.Controls.Add(Me.grpInput)
+        Me.Controls.Add(Me.menuHome)
+        Me.MainMenuStrip = Me.menuHome
+        Me.Name = "Main_Home"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Bureau de Change"
+        Me.grpInput.ResumeLayout(False)
+        Me.grpInput.PerformLayout()
+        Me.menuHome.ResumeLayout(False)
+        Me.menuHome.PerformLayout()
+        Me.grpOutput.ResumeLayout(False)
+        Me.grpOutput.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents grpInput As System.Windows.Forms.GroupBox
+    Friend WithEvents menuHome As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents grpOutput As System.Windows.Forms.GroupBox
+    Friend WithEvents btnReset As System.Windows.Forms.Button
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents cmbTitle As System.Windows.Forms.ComboBox
+    Friend WithEvents txtuserFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lbluserSecondName As System.Windows.Forms.Label
+    Friend WithEvents txtuserSurname As System.Windows.Forms.TextBox
+
+End Class
---
+++ Currency Converter/Main_Home.vb
@@ -0,0 +1,23 @@
+﻿Public Class Main_Home
+
+    Sub Currency_Convertor()
+
+        Dim userForename As String
+        Dim userSurname As String
+        Dim userAddress As String
+        Dim userPostcode As String
+
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseToolStripMenuItem.Click
+
+        Application.Exit()
+
+    End Sub
+
+    Private Sub btnReset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnReset.Click
+
+        Application.Restart()
+
+    End Sub
+End Class
